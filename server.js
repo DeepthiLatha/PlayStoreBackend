3dd59c98557f5d1a3575efaf1980b330574e3bb5
DeepthiLatha <kathuladeepthilatha10@gmail.com>
2024-04-29 14:27:36 +0530

    Removed space

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use(cors());
 
 const url = process.env.URI;
 
+
+
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Failed to connect to MongoDB:', err));
@@ -51,6 +53,7 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   
 
 const PORT = process.env.PORT;
+// const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
