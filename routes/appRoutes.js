456f0130e7a9c0ad0cd2960e146e45fd4e42fa9b
SaiDrive <mail2saishankar@gmail.com>
2024-05-06 23:12:22 +0530

    Updated comments for all App end Points

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -2,24 +2,29 @@ const express = require('express');
 const router = express.Router();
 
 const { authorize } = require('../middlewares/auth');
-const { getApps, createApp, getApp, updateApp, deleteApp, announceUpdates} = require('../handlers/app');
+const { getApps, createApp, getApp, updateApp, deleteApp, announceUpdates, createComment} = require('../handlers/app');
 
-// Get all applications for the logged-in owner and user
+// Endpoint to Get all applications for the logged-in owner and user
 router.get('/apps', authorize(), getApps);
 
-// Create a new application for Owner
+// Endpoint to Create a new application for Owner
 router.post('/createApp', authorize('ADMIN'), createApp);
 
-// Get a specific application 
+// Endpoint to Get a specific application 
 router.get('/apps/:id', authorize('ADMIN'), getApp);
 
-// Update an application for Owner
+// Endpoint to Update an application for Owner
 router.put('/apps/:id', authorize('ADMIN'), updateApp);
 
-// Delete an application for Owner
+// Endpoint to Delete an application for Owner
 router.delete('/apps/:id', authorize('ADMIN'), deleteApp);
 
-// Announce updates to users for Owner
+// Endpoint to Announce updates to users for Owner
 router.post('/apps/:id/announce', authorize('ADMIN'), announceUpdates);
+
+// Endpoint to create a comment and update the application's rating
+router.post('/comments', authorize('USER'), createComment);
+
+
 
 module.exports = router;
