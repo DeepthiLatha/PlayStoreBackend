671caa927728a83cd9eb9b7d3c513844a390ed79
SaiDrive <mail2saishankar@gmail.com>
2024-05-06 18:14:31 +0530

    changed route name and updated commnets for routes

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const { authorize } = require('../middlewares/auth');
+
 const { getApps, createApp, getApp, updateApp, deleteApp, announceUpdates} = require('../handlers/app');
 
-// Get all applications for the logged-in owner
+// Get all applications for the logged-in owner and user
 router.get('/apps', authorize(), getApps);
 
-// Create a new application
-router.post('/apps', authorize('ADMIN'), createApp);
+// Create a new application for Owner
+router.post('/createApp', authorize('ADMIN'), createApp);
 
-// Get a specific application
+// Get a specific application 
 router.get('/apps/:id', authorize('ADMIN'), getApp);
 
-// Update an application
+// Update an application for Owner
 router.put('/apps/:id', authorize('ADMIN'), updateApp);
 
-// Delete an application
+// Delete an application for Owner
 router.delete('/apps/:id', authorize('ADMIN'), deleteApp);
 
-// Announce updates to users
+// Announce updates to users for Owner
 router.post('/apps/:id/announce', authorize('ADMIN'), announceUpdates);
 
 module.exports = router;
